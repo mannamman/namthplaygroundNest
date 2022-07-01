d880536edaa7b77e8ebab1427a43c17566750638
namth <wase894@gmail.com>
2022-07-01 16:43:38 +0900

    test: log 테스트

--- auth-server/src/middlewares/req.log.middleware.ts
+++ auth-server/src/middlewares/req.log.middleware.ts
@@ -5,13 +5,13 @@ import { Request, Response, NextFunction } from 'express';
 export class ReqLoggerMiddleware implements NestMiddleware {
   constructor(private readonly logger: Logger) {}
 
-  use(request: Request, response: Response, next: NextFunction): void {
-    const { ip, method, path: url } = request;
-    const userAgent = request.get('user-agent') || '';
+  use(req: Request, res: Response, next: NextFunction): void {
+    const { ip, method, path: url } = req;
+    const userAgent = req.get('user-agent') || '';
 
-    response.on('close', () => {
-      const { statusCode } = response;
-      const contentLength = response.get('content-length');
+    res.on('close', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('content-length');
 
       this.logger.log(
         `${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
