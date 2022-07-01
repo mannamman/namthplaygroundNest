6a6c3b9519d1c9610306e9dc63445863e2570ce5
namth <wase894@gmail.com>
2022-07-01 15:58:17 +0900

    docs: logrotate 참고자료

--- yfin-server/graceful_restart.sh
+++ yfin-server/graceful_restart.sh
@@ -1,2 +1,5 @@
 pid=$(systemctl show --property MainPID --value yfinServer.service)
+# https://docs.gunicorn.org/en/stable/signals.html
+# Reload the configuration
+# The HUP signal can be used to reload the Gunicorn configuration on the fly.
 kill -SIGHUP ${pid}
